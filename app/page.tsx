f2bf5c23600e5d7e74bef73de1bb70f28c976bed
Алексей Колмаков <kaneco2013@yandex.ru>
2026-05-25 18:08:26 +0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-7xl font-bold tracking-widest">
+        EMBERHOME
+      </h1>
+    </main>
+  );
+}
