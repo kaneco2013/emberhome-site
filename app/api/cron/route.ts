22f6082a6a3eecb8dab2d00ed51c7a9eda58a208
Алексей Колмаков <kaneco2013@yandex.ru>
2026-06-16 11:26:01 +0300

    меняем логику на саппорт пейдже

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -31,24 +31,24 @@ export async function POST(request: Request) {
         'Authorization': `Bearer ${process.env.BOOSTY_ACCESS_TOKEN}`,
         'Accept': 'application/json',
         'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-        'Origin': 'https: //boosty.to',
-        'Referer': 'https: //boosty.to/',
+        'Origin': 'https://boosty.to',
+        'Referer': 'https://boosty.to/',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
       };
 
       // Делаем параллельные запросы. Адрес donators исправили на лорный боевой donations!
       const [targetResponse, subscribersResponse, donatorsResponse] = await Promise.all([
-        fetch(`https: //api.boosty.to/v1/blog/emberhome/target`, {
+        fetch(`https://api.boosty.to/v1/blog/emberhome/target`, {
           cache: 'no-store',
           headers: boostyHeaders,
           next: { revalidate: 0 },
         }),
-        fetch(`https: //api.boosty.to/v1/blog/emberhome/subscribers?limit=100`, {
+        fetch(`https://api.boosty.to/v1/blog/emberhome/subscribers?limit=100`, {
           cache: 'no-store',
           headers: boostyHeaders,
           next: { revalidate: 0 },
         }),
-        fetch(`https: //api.boosty.to/v1/blog/emberhome/donations?limit=100`, {
+        fetch(`https://api.boosty.to/v1/blog/emberhome/donations?limit=100`, {
           cache: 'no-store',
           headers: boostyHeaders,
           next: { revalidate: 0 },
